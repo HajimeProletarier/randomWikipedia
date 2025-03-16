c5ac31df51630ba56ef3859f50628fe8db4c8ece
Hajime <rbakk3ibz@yahoo.co.jp>
2025-03-17 00:15:41 +0900

    fix: entrypoint.sh のフォーマットを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-exec /usr/local/tomcat/bin/catalina.sh run \
-    -DDB_HOST=${DB_HOST} \
-    -DDB_PORT=${DB_PORT} \
-    -DDB_NAME=${DB_NAME} \
-    -DDB_USERNAME=${DB_USERNAME} \
-    -DDB_PASSWORD=${DB_PASSWORD}
+exec /usr/local/tomcat/bin/catalina.sh run -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -DDB_NAME=$DB_NAME -DDB_USERNAME=$DB_USERNAME -DDB_PASSWORD=$DB_PASSWORD
+
